dab6e375295c676bf03bebe7183264b6c6d1a27c
Eliseu <eliseutorres.gx@gmail.com>
2019-10-29 12:54:30 -0300

    Update queue_manager.py

--- queue_manager.py
+++ queue_manager.py
@@ -1,13 +1,13 @@
 # Copyright 2018 Eliseu Silva Torres
 #
-# This file is part of BAMSDN.
+# This file is part of QueueManager.
 #
 # BANSDN is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# BANSDN is distributed in the hope that it will be useful,
+# QueueManager is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
